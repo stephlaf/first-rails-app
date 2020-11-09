d92d51aa50bcb83e259b4ce5b3ad4fbd64114644
Stephane Lafontaine <stephane@medqigong.ca>
2020-11-09 09:53:51 -0500

    Added pages controller, about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 end
